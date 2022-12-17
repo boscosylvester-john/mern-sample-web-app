7e075ae6a5d76be4378e1a590e294311fbb790e0
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-17 15:30:01 -0500

    Add workouts router

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const workoutsRouter = require("./routes/workouts")
 
 // express app
 const app = express()
@@ -11,9 +12,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.get("/", (req, res) => {
-	res.json({msg: "Welcome!"})
-})
+app.use("api/workouts", workoutsRouter)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
