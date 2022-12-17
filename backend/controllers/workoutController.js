76201086e8a15ce20eb270510a25159b8913dfa9
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-17 17:07:03 -0500

    Add get controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,11 +1,26 @@
 const WorkoutModel = require("../models/workoutModel")
+const mongoose = require("mongoose")
 
 const getAllWorkouts = async (req, res) => {
+	const workouts = await WorkoutModel.find({}).sort({createdAt: -1})
 
+	res.status(200).json(workouts)
 }
 
 const getSingleWorkout = async (req, res) => {
+	const {id} = req.params
 
+	if(!mongoose.Types.ObjectId.isValid(id)){
+		return res.status(404).json({error: "No such workout"})
+	}
+
+	const workout = await WorkoutModel.findById(id)
+
+	if(!workout){
+		return res.status(404).json({error: "No such workout"})
+	}
+
+	res.status(200).json(workout)
 }
 
 const createWorkout = async (req, res) => {
@@ -14,7 +29,7 @@ const createWorkout = async (req, res) => {
 		const workoutObj = await WorkoutModel.create({title, reps, load})
 		res.status(200).json(workoutObj)
 	} catch(error){
-		res.status(400).json({"error": error.message})
+		res.status(400).json({error: error.message})
 	}
 }
 
