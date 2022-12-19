5290c6da13a68fa64b3901e97e2fd7b0aa00f796
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-18 20:21:00 -0500

    Update workout controller for remaining functions

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -34,11 +34,29 @@ const createWorkout = async (req, res) => {
 }
 
 const deleteWorkout = async (req, res) => {
+	const {id} = req.params
 
+	if(!mongoose.Types.ObjectId.isValid(id)){
+		return res.status(404).json({error: "No such workout"})
+	}
+
+	const workout = WorkoutModel.findOneAndDelete({_id: id})
+
+	res.status(200).json(workout)
 }
 
 const updateWorkout = async (req, res) => {
+	const {id} = req.params
 
+	if(!mongoose.Types.ObjectId.isValid){
+		return res.status(404).json({error: "No such workout"})
+	}
+
+	const workout = WorkoutModel.findOneAndUpdate({_id: id}, {
+		...req.body
+	})
+
+	res.status(200).json(workout)
 }
 
 module.exports = {
