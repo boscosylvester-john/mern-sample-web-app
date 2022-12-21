92cdcc1ed6b709103d8aa8efb14b55f8ce96ad0e
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-21 15:18:03 -0500

    Display data in frontend from backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
 
-	useEffect(() => {
-		const fetchWorkouts = async () => {
-			const workouts = await axios.get("/api/workouts")
-			console.log("got workouts: ", workouts.data)
-		}
+	const [workouts, setWorkouts] = useState([])
 
-		fetchWorkouts()
+	useEffect(() => {
+		try{
+			const fetchWorkouts = async () => {
+				const allWorkouts = await axios.get("/api/workouts")
+				console.log("got workouts: ", allWorkouts.data)
+				setWorkouts(allWorkouts.data)
+			}
+	
+			fetchWorkouts()
+		}
+		catch(error) {
+			console.log("Error: ", error)
+			setWorkouts([])
+		}
+		
 	}, [])
 
 	return (
 		<div className="home">
 			<h2>Home Page</h2>
+			{
+				workouts && (
+					workouts.map((wk, idx) => {
+						return (
+							<p key={idx}>
+								Workout: {wk.title} <br/>
+								Reps: {wk.reps} <br/>
+								Load: {wk.load} <br/>
+							</p>
+						)	
+					})
+				)
+			}
 		</div>
 	)
 }
